80b53a94ca38065ff81ed7abf1b144956e1126eb
Arthur Geel <ageel@enrise.com>
2023-09-22 12:40:46 +0200

    Remove Dashboard, add Chirps branding

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Chirper</title>
+        <title>{{ config('app.name') }}</title>
+        <link rel="icon" href="favicon.png" type="image/x-icon" />
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -15,20 +16,6 @@
     </head>
     <body class="antialiased">
         <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-slate-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-            @if (Route::has('login'))
-                <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
-                    @auth
-                        <a href="{{ url('/chirps') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Chirps</a>
-                    @else
-                        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
             <div class="max-w-7xl mx-auto p-6 lg:p-8">
                 <div class="flex items-center gap-4">
                     <x-application-logo class="h-24 fill-current text-red-500 w-auto" />
@@ -39,7 +26,7 @@
                 </div>
 
                 @if (Route::has('login'))
-                    <div class="flex mt-16 justify-center items-center gap-6">
+                    <div class="flex mt-8 sm:mt-16 justify-center items-center gap-6">
                     @auth
                     <a href="{{ url('/chirps') }}" class="px-4 py-2 bg-red-500 text-white">Show me those Chirps!</a>
                     @else
