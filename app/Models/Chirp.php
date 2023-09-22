8870e76fc71be10b3ef30fa6414c625403b5a34f
Arthur Geel <ageel@enrise.com>
2023-09-22 11:02:27 +0200

    Feature: Send Email notifications for new Chirps

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -2,23 +2,27 @@
 
 namespace App\Models;
 
+use App\Events\ChirpCreated;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Chirp extends Model
 {
+    use HasFactory;
 
-    // Mass Asignment is blocked by default in Laravel.
+    // @NOTE: Mass Asignment is blocked by default in Laravel.
     // 👇 We can enable mass assignment for safe attributes by marking them as "fillable"
     protected $fillable = [
         'message'
+    ];
+
+    protected $dispatchesEvents = [
+        'created' => ChirpCreated::class,
     ];
 
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
-
-    use HasFactory;
 }
