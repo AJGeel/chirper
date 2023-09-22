38e4c8aad807d385bcdacb409431155d843511f1
Arthur Geel <ageel@enrise.com>
2023-09-22 13:04:06 +0200

    Update welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,20 +15,20 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="antialiased">
-        <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-slate-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-            <div class="max-w-7xl mx-auto p-6 lg:p-8">
+        <div class="relative flex justify-center items-center min-h-screen bg-dots-darker bg-center bg-slate-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
+            <div class="max-w-7xl mx-auto p-6 lg:p-8 z-10">
                 <div class="flex items-center gap-4">
                     <x-application-logo class="h-24 fill-current text-red-500 w-auto" />
                     <div>
                         <h1 class="text-4xl font-extrabold text-red-500">Chirper</h1>
-                        <p class="text-gray-600">A micro messaging platform for <span class="text-red-500">web artisans</span>.</p>
+                        <p class="text-gray-600">A micro messaging platform for <span class="text-red-500">Web Artisans</span>.</p>
                     </div>
                 </div>
 
                 @if (Route::has('login'))
-                    <div class="flex mt-8 sm:mt-16 justify-center items-center gap-6">
+                    <div class="flex mt-8 justify-center items-center gap-6">
                     @auth
-                    <a href="{{ url('/chirps') }}" class="px-4 py-2 bg-red-500 text-white">Show me those Chirps!</a>
+                    <x-secondary-link href="{{ url('/chirps') }}">{{ __('Show me those Chirps!') }}</x-primary-link>
                     @else
                     <x-primary-link href="{{ route('login') }}">
                         {{ __('Log In') }}
@@ -43,6 +43,10 @@
                     @endauth
                 </div>
                 @endif
+            </div>
+
+            <div class="absolute z-0 opacity-50">
+                <div class="w-80 h-80 rounded-full bg-red-500 animate-pulse opacity-5" style="filter: blur(100px)"/>
             </div>
         </div>
     </body>
